6305ac6b5391f7002fc8c4ec452bc5f23f1f02dc
Edgars Skrabins <shiftkiro@gmail.com>
2024-10-29 15:01:26 +0200

    Valid drop circumstance

--- Assets/Scripts/BodyPart.cs
+++ Assets/Scripts/BodyPart.cs
@@ -2,5 +2,8 @@ using UnityEngine;
 
 public class BodyPart : PickUpAble
 {
-
+    protected override bool AreDropCircumstancesValid()
+    {
+        return true;
+    }
 }
--- Assets/Scripts/Templates/PickUpAble.cs
+++ Assets/Scripts/Templates/PickUpAble.cs
@@ -1,17 +1,41 @@
+using System;
 using UnityEngine;
 
 public class PickUpAble : MonoBehaviour
 {
     [SerializeField] protected GameObject pickupUI;
+    public event Action OnItemDropSuccess;
+    public event Action OnItemDropFailed;
     public virtual void PickUp(Transform parent, Vector3 pickupPosition)
     {
         transform.SetParent(parent);
         transform.position = pickupPosition;
     }
 
-    public virtual void Drop()
+    public virtual void AttemptDrop()
+    {
+        if (AreDropCircumstancesValid())
+        {
+            Drop();
+            return;
+        }
+
+        HandleFailedDropAttempt();
+    }
+    protected virtual void Drop()
     {
         transform.SetParent(null);
+        OnItemDropSuccess?.Invoke();
+    }
+
+    protected virtual bool AreDropCircumstancesValid()
+    {
+        return true;
+    }
+
+    protected virtual void HandleFailedDropAttempt()
+    {
+        OnItemDropFailed?.Invoke();
     }
 
     public virtual void ShowPickUpUI()
